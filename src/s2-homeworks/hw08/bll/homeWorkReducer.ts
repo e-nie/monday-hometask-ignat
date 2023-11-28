bd6e86d4d3b846ae66b6fd1c93ce9eca6ca92609
e-nie <99044956+e-nie@users.noreply.github.com>
2023-11-28 01:12:38 +0100

    add hw-8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
+            if (action.payload === 'up') {
+                return [...state].sort((a, b) => a.name.localeCompare(b.name))
+            } else if (action.payload === 'down') {
+                return [...state].sort((a, b) => b.name.localeCompare(a.name))
+            }
             return state // need to fix
         }
         case 'check': {
+            return [...state].filter(el => el.age >= 18)
 
-            return state // need to fix
         }
         default:
             return state
